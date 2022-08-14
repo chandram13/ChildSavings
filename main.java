cbecf65a6e303099aaee41fba28952e16240d25c
chandram13 <mschandr@uci.edu>
2022-08-14 02:12:18 -0700

    Initial commit

---
+++ main.java
@@ -0,0 +1,13 @@
+
+
+
+public class calcCompound {
+    public static void compoundInterest (String [] args)
+        insertbaseFormula
+    public static void compoundInterest publicSchoool (String [] args)
+        insertbasepublic
+    public static void compoundInterest privateSchool (String [] args)
+        insertbaseprivate
+    public static void compountInterest tradeSchool (String [] args)
+        insertbasetrade
+}
