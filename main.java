8f5d39bf978feb47ca979ec864b4e1cb325d8c61
chandram13 <mschandr@uci.edu>
2022-08-14 02:26:25 -0700

    Updated the n values of all function scenarios except for  base.

--- main.java
+++ main.java
@@ -1,13 +1,13 @@
-
+// Marvish Chandra
 
 
 public class calcCompound {
-    public static void compoundInterest (String [] args)
-        insertbaseFormula
-    public static void compoundInterest publicSchoool (String [] args)
-        insertbasepublic
-    public static void compoundInterest privateSchool (String [] args)
-        insertbaseprivate
-    public static void compountInterest tradeSchool (String [] args)
-        insertbasetrade
+    public static void compoundInterest (String []  P, r, n, t)
+        myA = P(1 + r/n)^nt
+    public static void compoundInterest publicSchoool (String [] P, r, n = 4, t)
+        basePublic = P(1 + r/n)^nt
+    public static void compoundInterest privateSchool (String [] P, r, n = 4, t)
+        basePrivate = P(1 + r/n)^nt
+    public static void compountInterest tradeSchool (String [] P, r, n = 2. t)
+        baseTrade = P(1 + r/n)^nt
 }
